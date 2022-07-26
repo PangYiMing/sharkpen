4dbfdd3fcb2da2f6c135bbdda8ff1d36de95de41
pang <7861445+redmapleleaf@user.noreply.gitee.com>
2022-07-26 23:43:47 +0800

    first commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,16 @@
+/*
+ * @Author: pym
+ * @Date: 2022-01-04 11:51:03
+ * @LastEditors: pym
+ * @Description:
+ * @LastEditTime: 2022-07-26 23:18:39
+ */
+import React from 'react';
+import './index.less';
+
+function Home() {
+  // fetch('/tripdocs/test/api', { method: 'POST' });
+  return <div>home</div>;
+}
+
+export default Home;
